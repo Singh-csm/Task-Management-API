ae2f36cfb108c7f974c2f7329ef4b99f9761846e
Singh-csm <csm8107@gmail.com>
2023-06-05 17:19:22 +0530

    project: init: mongoose,express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route")
+const app = express();
+const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv")
+dotenv.config()
+const MONGO_URI = process.env.MONGO_URI;
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_URI, {useNewUrlParser: true})
+.then(()=>console.log("MongoDB is Connected"))
+.catch(err=>console.log(err))
+app.use("/", route)
+
+app.get("/*", (req, res)=>{
+    res.status(404).send({status:false, message:"Not Found"});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on PORT: ${PORT}...`);
+})
